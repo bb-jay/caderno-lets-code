15dc2adbbd764f96f158dc3f2a0fd96edd9b8036
F3879710 Gustavo de Magalhaes Barbosa <gustavo.jay@bb.com.br>
2022-06-28 16:00:41 -0300

    e3q2 pronto

--- cerberus/src/main/java/br/com/letscode/Exercicios/E3Q2.java
+++ cerberus/src/main/java/br/com/letscode/Exercicios/E3Q2.java
@@ -2,19 +2,28 @@ package br.com.letscode.Exercicios;
 
 public class E3Q2 {
 	public static void main(String[] args) {
-		
+		accountType("PERSONAL");
+		accountType("Personal");
+		accountType("BUSINESS");
+		accountType("BUsInesS");
+		accountType("banana");
+		accountType("chuveiro");
 	}
 
 	public static void accountType (String accountType) {
 		
+		accountType = accountType.toUpperCase();
+
 		switch(accountType) {
 			case "PERSONAL":
 				System.out.println("Conta pessoal");
+				break;
 			case "BUSINESS":
-				System.out.println("Conta corporativa");
+				System.out.println("Conta empresarial");
+				break;
 			default:
-
+				System.out.println("Tipo de conta não existente");
 		}
-		
+
 	}
 }
