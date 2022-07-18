e71ec91ed389b4610dfe6651fb131a54ccf25e29
F3879710 Gustavo de Magalhaes Barbosa <gustavo.jay@bb.com.br>
2022-07-18 13:04:37 -0300

    Teste Veiculos do rascunho para Aula._15_07

---
+++ cerberus/src/main/java/br/com/letscode/Aulas/_15_07_InterfaceEClasseAbstrata/TesteVeiculos.java
@@ -0,0 +1,21 @@
+package br.com.letscode.Aulas._15_07_InterfaceEClasseAbstrata;
+
+public class TesteVeiculos {
+	public static void main(String[] args) {
+		Carro c = new Carro(150, "OMM-9492", "Preto");
+		c.buzinar();
+
+		Moto m = new Moto(30, "OMM-9491", "Vermelho");
+		m.buzinar();
+
+		System.out.println();
+
+		c.trocarEstacaoRadio(200.0);
+		c.ligarRadio();
+		c.ligarRadio();
+		c.trocarEstacaoRadio(50.0);
+		c.trocarEstacaoRadio(150.0);
+		c.desligarRadio();
+		c.desligarRadio();
+	}
+}
